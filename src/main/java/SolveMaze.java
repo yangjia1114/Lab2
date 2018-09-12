e891e94b10b94f968a43ac303efd012343fdd643
Yang Jia <jiay3@illinois.edu>
2018-09-12 16:07:56 -0500

    lab

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,13 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                while (!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.move();
+            }
         }
 
         if (maze.isFinished()) {
